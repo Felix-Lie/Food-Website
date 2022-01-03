af89b0abf044f2dec3ffc7f2dc3cf7d8a3262771
Felix lie <felixlie19@gmail.com>
2022-01-02 22:20:06 -0800

    Create & Style Navbar

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelectorAll('.target').forEach((item) => {
+    item.classList.toggle('change');
+  });
+});
+
 const icons = document.querySelectorAll('.section-1-icons i');
 let i = 1;
 
@@ -14,4 +20,4 @@ setInterval(() => {
   } else {
     icon.nextElementSibling.classList.add('change');
   }
-}, 4000);
+}, 3000);
