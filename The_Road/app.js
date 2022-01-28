549212918a99579ce1a693d60902f95bdeb40384
Felix lie <felixlie19@gmail.com>
2022-01-27 21:51:38 -0800

    Create and Style Navbar

--- The_Road/app.js
+++ The_Road/app.js
@@ -1,4 +1,22 @@
 const navButton = document.querySelectorAll('.navigation-button');
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+const navLink = document.querySelectorAll('.nav-link');
+
+const container = document.querySelector('.container');
+
+document.querySelector('.open-navbar-icon').addEventListener('click', () => {
+  container.classList.add('change');
+});
+
+document.querySelector('.close-navbar-icon').addEventListener('click', () => {
+  container.classList.remove('change');
+});
+
+let i = 0;
+
+navLink.forEach((item) => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
 
 Array.from(
   navButton.forEach((item) => {
